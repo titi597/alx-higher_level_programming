2cd58b80d4d429453493b9cfc28598476bd354f5
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-28 03:52:37 -0800

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 combinations = [
-        f"{i}{j}\n" if i == 8 and j == 9 else f"{i}{j}, "
-        for i in range(9)
-        for j in range(i + 1, 10)
-        ]
+    "{}{}\n".format(i, j) if i == 8 and j == 9 else "{}{}, ".format(i, j)
+    for i in range(9)
+    for j in range(i + 1, 10)
+]
 print("".join(combinations), end="")
