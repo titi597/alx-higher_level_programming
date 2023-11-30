6220a0efb58c9f05550a38c8add78a65f5710837
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-30 04:55:33 -0800

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
     add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub')).add, __import__('magic_calculation_102', fromlist=('add', 'sub')).sub
     if a < b:
