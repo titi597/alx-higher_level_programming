6269fe0e4f19b5fa14ff7e9f0401f982c32913ea
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-30 02:43:04 -0800

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # my_calculating_program.py
 
-a = 10
-b = 5
-
-from calculator_1 import add, sub, mul, div
-
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+if __name__ == "__main__":
+    """Print the addition, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
