dd598122c3594bcc3b2e69991289b67cfb5172ec
titi597 <139129034+titi597@users.noreply.github.com>
2023-11-30 05:07:16 -0800

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub')).add, __import__('magic_calculation_102', fromlist=('add', 'sub')).sub
+    """Matching bettycode made by Alx."""
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
