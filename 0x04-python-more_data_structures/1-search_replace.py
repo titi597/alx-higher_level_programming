779b99fc0f215768066ff2755e5c9948560f1731
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-06 01:22:15 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    # Create a new list to store the modified elements
+    new_list = []
+
+    # Iterate through the elements of the original list
+    for element in my_list:
+        # Replace occurrences of 'search' with 'replace'
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+
+    return new_list
