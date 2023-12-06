528222815a419e9b669a8cb2bf3526f435d734be
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-06 01:43:58 -0800

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Check if the key already exists in the dictionary
+    if key in a_dictionary:
+        # If the key exists, update the value
+        a_dictionary[key] = value
+    else:
+        # If the key doesn't exist, add a new key-value pair
+        a_dictionary[key] = value
+
+    return a_dictionary
