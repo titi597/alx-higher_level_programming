01af19afc5c742f9290a98092c57384ee88a7497
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-18 05:53:32 -0800

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
-
 import sys
 
-
-def safe_print_integer_err(value):
-    """Printing an integer with "{:d}".format().
-
-    If a ValueError message is caught, a corresponding
-    message is printed to standard error.
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        the result of the function, Otherwise, returns
-        None if something.
-    """
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except (TypeError, ValueError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return False
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
