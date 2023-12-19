3cd02f02093b653b2bba1c9317651f9fd223fc0d
titi597 <139129034+titi597@users.noreply.github.com>
2023-12-19 04:47:20 -0800

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,31 +2,25 @@
 
 """Python class MagicClass"""
 
-
 import math
 
 class MagicClass:
     """fucntioning magic class"""
 
     def __init__(self, radius=0):
-        """initializing
-        Args:
-        radius: length of side of the square.
+        """initializing.
+        Arg:
+            radius (float or int: length of side of the square.
         """
-
         self.__radius = 0
-
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-
         self.__radius = radius
 
     def area(self):
         """displaying area"""
-
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """displayingperimeter"""
-
         return 2 * math.pi * self.__radius
