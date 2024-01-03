a95234ed9f635850cd7725cb25a7e7d72ba5137a
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-03 04:32:59 -0800

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """Say my name."""
 def say_my_name(first_name, last_name=""):
-    """a function that prints My name is <first name> <last name>."""
-    # Check if first_name and last_name are strings
-    if not (isinstance(first_name, str) and isinstance(last_name, str)):
+    """
+    Prints a formatted string with the provided first and last names.
+
+    Parameters:
+    - first_name (str): The first name.
+    - last_name (str, optional): The last name. Default is an empty string.
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    # Print the formatted message
-    print("My name is {} {}".format(first_name, last_name))
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
