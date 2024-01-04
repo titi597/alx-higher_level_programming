cf73a379b22f3f9705e6e51928884432e2823ee5
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-04 02:32:32 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,17 +3,23 @@
 
 
 class Rectangle:
-    """defines a rectangle by: (based on 5-rectangle.py)."""
+    """defines a rectangle by: (based on 5-rectangle.py).
+    
+    Attributes:
+            number_of_instances (int): the number of Rectangle instances.
+    """
 
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        """Initializing."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """setting the width of rectangle."""
         return self.__width
 
     @width.setter
@@ -26,6 +32,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """setting the height of the rectangle."""
         return self.__height
 
     @height.setter
@@ -37,20 +44,25 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """returning area."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """returning the perimeter."""
         return 2 * (self.__width + self.__height) if self.__width > 0 and self.__height > 0 else 0
 
     def __str__(self):
+        """returning the printable rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
             return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
+        """returning the string of the rectangle."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
+        """printing message for deletion of rectangle."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
