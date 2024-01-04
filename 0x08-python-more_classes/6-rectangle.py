db89de716330f87e83179b7c04287532d8a0e911
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-04 03:47:21 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -76,5 +76,5 @@ class Rectangle:
 
     def __del__(self):
         """printing message for deletion of rectangle."""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
