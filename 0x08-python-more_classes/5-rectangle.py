8e73b77164703baa5ac116c4291bd2ee11f2ca15
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-04 03:34:53 -0800

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -37,7 +37,9 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.__width + self.__height) if self.__width > 0 and self.__height > 0 else 0
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
