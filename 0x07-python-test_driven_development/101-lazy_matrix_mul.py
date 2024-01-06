565f7a53fba110f10fb9d1bcf550dc46a3eb63f3
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-06 02:50:31 -0800

    7. Lazy matrix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,6 +2,7 @@
 """Lazy matrix multiplication."""
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using NumPy.
@@ -16,18 +17,24 @@ def lazy_matrix_mul(m_a, m_b):
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("m_a must be a list or m_b must be a list")
 
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+    if not all(isinstance(row, list) for row in m_a) or \
+            not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or \
+                m_b must be a list of lists")
 
-    if not m_a or not m_b or not all(row for row in m_a) or not all(row for row in m_b):
+    if not m_a or not m_b or not all(row for row in m_a) or \
+            not all(row for row in m_b):
         raise ValueError("m_a can't be empty or m_b can't be empty")
 
-    if not all(isinstance(element, (int, float)) for row in m_a for element in row) or \
-       not all(isinstance(element, (int, float)) for row in m_b for element in row):
-        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+    if not all(isinstance(el, (int, float)) for row in m_a for el in row) or \
+       not all(isinstance(el, (int, float)) for row in m_b for el in row):
+        raise TypeError("m_a should contain only integers or floats or \
+                m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
-        raise ValueError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+    if not all(len(row) == len(m_a[0]) for row in m_a) or \
+            not all(len(row) == len(m_b[0]) for row in m_b):
+        raise ValueError("each row of m_a must be of the same size or \
+                each row of m_b must be of the same size")
 
     np_a = np.array(m_a)
     np_b = np.array(m_b)
