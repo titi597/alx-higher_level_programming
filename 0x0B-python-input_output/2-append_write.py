88282397cad08639958100b40bcf159c8e5d6742
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-09 10:53:33 -0800

    2. Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,4 @@
 
 def append_write(filename="", text=""):
     with open(filename, 'a', encoding='utf-8') as file:
-        nb_characters_added = file.write(text)
-    return nb_characters_added
+        return file.write(text)
