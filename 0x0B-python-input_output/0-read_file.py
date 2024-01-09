2627caa6093496a1902e5c6f9b5818faa307e115
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-09 10:14:38 -0800

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,5 @@
 
 def read_file(filename=""):
     """a function that reads a text file (UTF8)."""
-
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content, end="")
-    except FileNotFoundError:
-        pass  # Do nothing if the file is not found
+    with open(filename, 'r', encoding='utf-8') as file:
+            print(file.read(), end="")
