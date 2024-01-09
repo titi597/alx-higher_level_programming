03f446e7629e03eef4b59fa279716095df148a49
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-09 10:18:37 -0800

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """a function that reads a text file (UTF8)."""
     with open(filename, 'r', encoding='utf-8') as file:
-            print(file.read(), end="")
+        print(file.read(), end="")
