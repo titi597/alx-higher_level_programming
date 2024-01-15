6e6a0bc4ffaf4311227394e3070bc75a23f2cb74
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-15 01:08:14 -0800

    10. And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module containing the Square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class, inherits from Rectangle."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for Square class
+
+        Args:
+            size (int): Size of the square
+            x (int): X-coordinate of the square (default is 0)
+            y (int): Y-coordinate of the square (default is 0)
+            id (int): Optional integer id (default is None)
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Override the __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width
+        )
