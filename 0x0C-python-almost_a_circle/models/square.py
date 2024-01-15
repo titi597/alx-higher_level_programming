24d5bf9d2a5c4c79fa4687defcc82a98eb1d3ff1
titi597 <139129034+titi597@users.noreply.github.com>
2024-01-15 01:18:40 -0800

    12. Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,6 +28,16 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Update attributes with the arguments or key-worded arguments"""
+        attrs = ["id", "size", "x", "y"]
+        if args:
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Override the __str__ method"""
         return "[Square] ({}) {}/{} - {}".format(
